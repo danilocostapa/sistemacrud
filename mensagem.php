8c2f8abaed2654795bfdbb4048fff6a2039366b3
danilocostapa <bass.danilo@gmail.com>
2025-12-11 15:20:51 -0300

    Correção na mensagem que mostra na tela index.php

--- mensagem.php
+++ mensagem.php
@@ -5,7 +5,7 @@ if (isset($_SESSION['mensagem'])):
 ?>
 
     <div class="alert alert-warning alert-dismissible fade show " role="alert">
-        <?php $_SESSION['mensagem']; ?>
+        <?= $_SESSION['mensagem']; ?>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 
